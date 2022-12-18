4eddab097827bf8a57d766bbab3011b3a6f8694d
Vojtech Mares <iam@vojtechmares.com>
2022-12-18 22:19:23 +0100

    refactor(prisma/seed.ts): use existing prisma instance

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import prisma from "@/lib/prisma";
 
 async function main() {
   const k8s = await prisma.training.create({
